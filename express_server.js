2a40aeda4949d1f87ce1feafac4672fe1e5fce64
deonleer <deon.leer96@gmail.com>
2020-11-11 22:12:04 +0000

    Added body-parser dependancy and a new URL form

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080;
+const bodyParser = require("body-parser");
 
 app.set("view engine", "ejs")
+app.use(bodyParser.urlencoded({extended: true}));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -13,23 +15,35 @@ function generateRandomString() {
   return Math.random().toString(36).substr(2,6);
 }
 
+//root directory says hello
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//json string of object
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
 
+//shows all URLs
 app.get("/urls/", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
 })
 
+
+//shows specific URL
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
   res.render("urls_show", templateVars);
 })
+
+//New URL submission form
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
