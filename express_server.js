53d77eb380a580d3c823a4206a2df66ea76648c2
deonleer <deon.leer96@gmail.com>
2020-11-12 00:05:48 +0000

    displays username

--- express_server.js
+++ express_server.js
@@ -17,7 +17,7 @@ function generateRandomString() {
   return Math.random().toString(36).substr(2,6);
 }
 
-//root directory says hello
+//hello
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
@@ -27,47 +27,78 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 })
 
+
+//Login
+app.post("/urls/login", (req, res) => {
+
+  res.cookie("username", req.body.username);
+
+  res.redirect("/urls");
+
+})
+
+
 //shows all URLs
 app.get("/urls/", (req, res) => {
-  const templateVars = {urls: urlDatabase};
+
+  const templateVars = {urls: urlDatabase, username: req.cookies["username"]};
+
   res.render("urls_index", templateVars);
 })
 
 //New URL submission form
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+
+  const templateVars= {username: req.cookies["username"]}
+
+  res.render("urls_new", templateVars);
 })
 
 //shows specific URL
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]};
+
+  const templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"]};
+
   res.render("urls_show", templateVars);
 })
 
 //redirects to longURL
 app.get("/u/:shortURL", (req, res) => {
+
   const longURL = urlDatabase[req.params.shortURL];
+
   res.redirect(longURL);
 });
 
 //Creates new Short URL and redirects to URL page
 app.post("/urls", (req, res) => {
+
   let newid = generateRandomString();
+
   urlDatabase[newid] = req.body["longURL"];
+
   res.redirect(`/urls/${newid}`);
 })
 
 //Deleted URLs from database
 app.post("/urls/:shortURL/delete", (req, res) => {
+
   delete urlDatabase[req.params.shortURL];
+
   res.redirect("/urls");
 })
 
+//Edit URL
 app.post("/urls/:shortURL", (req, res) => {
-  const templateVars = {shortURL: req.params.shortURL, longURL: req.body.editURL};
+
+  const templateVars = {shortURL: req.params.shortURL, longURL: req.body.editURL, username: req.cookies["username"]};
+
   urlDatabase[req.params.shortURL] = req.body.editURL;
+
   res.render("urls_show", templateVars);
 });
+
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
